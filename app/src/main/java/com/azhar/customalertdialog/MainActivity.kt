6d3622fcdd39bffc61865562e91de89faca8617e
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2021-11-28 23:52:46 +0600

    Initial commit

---
+++ app/src/main/java/com/azhar/customalertdialog/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.azhar.customalertdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_view.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_show.setOnClickListener {
+
+            val view = View.inflate(this@MainActivity, R.layout.dialog_view, null)
+
+            val builder = AlertDialog.Builder(this@MainActivity)
+            builder.setView(view)
+
+            val dialog = builder.create()
+            dialog.show()
+            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+            dialog.setCancelable(false)
+
+            view.btn_confirm.setOnClickListener {
+                dialog.dismiss()
+
+            }
+
+
+        }
+    }
+}
